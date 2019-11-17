1f849a41c28c5300188eaf5e715977696f2c9784
Jeremy Kenny <jeremy.kenny@mail.utoronto.ca>
2019-11-17 13:28:51 -0500

    Added resource to return beta test link at the /beta endpoint.

--- web.py
+++ web.py
@@ -1,10 +1,12 @@
 import falcon
 from events import Events
+from beta import BetaLinkResource
 
 # Connect all the resources to their endpoints.
 # This file is a nice place to see the whole API
 
 api = falcon.API()
 api.add_route('/events/{category}', Events())
+api.add_route('/beta', BetaLinkResource())
 
 # Whatever is passed in place of {category} enters as a variable
