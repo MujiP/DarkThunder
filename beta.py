a0f9015454115d04b010f0df83d25b79d7a01c72
Jeremy Kenny <jeremy.kenny@mail.utoronto.ca>
2019-11-17 13:52:08 -0500

    Put the link inside a JSON object

--- beta.py
+++ beta.py
@@ -1,5 +1,6 @@
 # beta.py
 import falcon
+import json
 
 class BetaLinkResource(object):
     def on_get(
@@ -7,4 +8,5 @@ class BetaLinkResource(object):
         req: falcon.Request,
         resp: falcon.Response):
         resp.status = falcon.HTTP_200
-        resp.body = (r'https://testflight.apple.com/join/e05hgsxP?fbclid=IwAR2Trl0qZ9pFvEaKjQq2aVvAwJC8IwjPUaEGrJJWls1HiER2MgjE-wCw0MM')
+        beta_link = {'link': r'https://testflight.apple.com/join/e05hgsxP?fbclid=IwAR2Trl0qZ9pFvEaKjQq2aVvAwJC8IwjPUaEGrJJWls1HiER2MgjE-wCw0MM'}
+        resp.body = (json.dumps(beta_link))
